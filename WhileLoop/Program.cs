0522644b3e897974583af51b870c26b9af9ffe12
Juan Manuel Rivera <jumariya@hotmail.com>
2025-04-04 22:24:34 -0500

    Patron

--- WhileLoop/Program.cs
+++ WhileLoop/Program.cs
@@ -17,22 +17,59 @@
             // 10 = 10x1,10x2 10x3.... 10x12
 
           
-            int contadortabla = 1;
+            //int contadortabla = 1;
 
-            Console.WriteLine("Ingrese el valior de la tabla de multiplciar");
+            //Console.WriteLine("IMPRIMIR PATRONES");
             
-            int valorTabla;
-            int resultado;
-            valorTabla = Convert.ToInt32(Console.ReadLine());
-            while (contadortabla <= 12)
-            {
-                Console.WriteLine("El numero es: " + contadortabla);
-                resultado = valorTabla * contadortabla;
-                Console.WriteLine("latabla del numero: " + valorTabla + " es igual a " + +valorTabla+ "x" + contadortabla + "="+ resultado);
-                contadortabla++;
+            //int valorTabla;
+            //int resultado;
+            //valorTabla = Convert.ToInt32(Console.ReadLine());
+            //while (contadortabla <= 12)
+            //{
+            //    Console.WriteLine("El numero es: " + contadortabla);
+            //    resultado = valorTabla * contadortabla;
+            //    Console.WriteLine("latabla del numero: " + valorTabla + " es igual a " + +valorTabla+ "x" + contadortabla + "="+ resultado);
+            //    contadortabla++;
+            //}
+
+
+            // Imprimir cuadro con patrones de 1 al 5
+            //int inicio = 1;
+
+            //while(inicio<=5)
+            //    {
+            //    int b = 1;
+            //    while (b <= 5)
+            //    {
+            //        Console.Write(b + " ");
+            //        b++;
+            //    }
+            //    Console.WriteLine(""); // salto de Linea
+
+            //    inicio++;
+
+
+                // Ejercicio de while loop
+
+                int inicio = 1;
+                while (inicio <= 5)
+                {
+                    int b = 1;
+                    while (b <= inicio) // Aquí limitamos hasta el valor actual de inicio
+                    {
+                        Console.Write(b + " ");
+                        b++;
+                    }
+                    Console.WriteLine(""); // Salto de línea
+                    inicio++;
+                }
+
+
             }
+
+
             
 
         }
-    }
+ 
 }
